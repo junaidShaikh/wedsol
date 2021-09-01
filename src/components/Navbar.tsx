30e757b476438e528c5c0d7f91257b0c1244dd64
SivaramPg <sivarampg95@gmail.com>
2021-09-01 16:25:05 +0530

    new `Navbar` component implementation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components/macro';
+
+import FlexRowWrapper from 'components/common/wrappers/FlexRowWrapper';
+
+import wedSolLogo from 'assets/logos/wedsol.png';
+
+const NavbarWrapper = styled.nav`
+  width: 100%;
+  height: 42px;
+
+  background: #ffffff;
+  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.04);
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  & > ${FlexRowWrapper} {
+    width: 100%;
+    height: 100%;
+
+    align-items: center;
+    justify-content: center;
+
+    .wedsol-logo {
+      width: 22px;
+      height: 22px;
+      margin-right: 13px;
+    }
+
+    p {
+      font-family: 'Inter';
+      font-weight: 500;
+      font-size: 15px;
+      line-height: 18px;
+      text-align: center;
+      letter-spacing: 0.505em;
+
+      color: #000000;
+    }
+  }
+`;
+
+const Navbar = (): JSX.Element => {
+  return (
+    <NavbarWrapper>
+      <FlexRowWrapper>
+        <img className="wedsol-logo" src={wedSolLogo} alt="wedsol" />
+        <p>WEDSOL</p>
+      </FlexRowWrapper>
+    </NavbarWrapper>
+  );
+};
+
+export default Navbar;
