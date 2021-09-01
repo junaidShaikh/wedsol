63e6f6fd5e91c3be0dcfea5235cca8236f909be2
SivaramPg <sivarampg95@gmail.com>
2021-09-01 19:24:35 +0530

    new solid button component implementation

---
+++ src/components/common/SolidButton.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components/macro';
+
+const SolidButton = styled.button`
+  width: 100%;
+  height: 50px;
+
+  background: #292735;
+  border-radius: 54px;
+
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 21px;
+
+  color: #ffffff;
+`;
+
+export default SolidButton;
