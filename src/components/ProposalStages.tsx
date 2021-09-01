55c16faae9498727e960e0749bdae12bfe87f57c
SivaramPg <sivarampg95@gmail.com>
2021-09-01 17:14:42 +0530

    new `ProposalStage` component implementation

---
+++ src/components/ProposalStages.tsx
@@ -0,0 +1,94 @@
+import styled from 'styled-components/macro';
+
+import FlexRowWrapper from './common/wrappers/FlexRowWrapper';
+import FlexColumnWrapper from './common/wrappers/FlexColumnWrapper';
+
+import sendARing from 'assets/images/send-a-ring.png';
+import registerMarriage from 'assets/images/register-marriage.png';
+
+const ProposalStagesWrapper = styled.div`
+  width: 622px;
+  height: 141px;
+
+  background: #ffffff;
+  border: 1px solid #e3e3e3;
+  border-radius: 13px;
+
+  & > ${FlexRowWrapper} {
+    height: 100%;
+
+    & > ${FlexColumnWrapper} {
+      flex: 0 1 50%;
+
+      justify-content: center;
+      align-items: center;
+
+      cursor: pointer;
+
+      &:first-of-type {
+        border-right: 1px solid #e3e3e3;
+      }
+
+      & > ${FlexRowWrapper} {
+        align-items: center;
+        justify-content: center;
+
+        .send-a-ring-image,
+        .register-marriage-image {
+          width: 65px;
+          height: 65px;
+          margin-left: 14px;
+          margin-right: 14px;
+        }
+
+        h4 {
+          font-weight: 500;
+          font-size: 20px;
+          line-height: 24px;
+
+          color: #060606;
+          margin-bottom: 4px;
+        }
+
+        p {
+          max-width: 165px;
+
+          font-weight: 500;
+          font-size: 18px;
+          line-height: 22px;
+
+          color: rgba(6, 6, 6, 0.4);
+        }
+      }
+    }
+  }
+`;
+
+const ProposalStages = (): JSX.Element => {
+  return (
+    <ProposalStagesWrapper>
+      <FlexRowWrapper>
+        <FlexColumnWrapper>
+          <FlexRowWrapper>
+            <img src={sendARing} alt="" className="send-a-ring-image" />
+            <div>
+              <h4>Send a ring</h4>
+              <p>Send a virtual ring & propose</p>
+            </div>
+          </FlexRowWrapper>
+        </FlexColumnWrapper>
+        <FlexColumnWrapper>
+          <FlexRowWrapper>
+            <img src={registerMarriage} alt="" className="register-marriage-image" />
+            <div>
+              <h4>Register Marriage</h4>
+              <p>Begin a new marriage forever</p>
+            </div>
+          </FlexRowWrapper>
+        </FlexColumnWrapper>
+      </FlexRowWrapper>
+    </ProposalStagesWrapper>
+  );
+};
+
+export default ProposalStages;
