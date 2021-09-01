9163adb65f674e901f35853a8248747c13252d86
SivaramPg <sivarampg95@gmail.com>
2021-09-01 19:24:50 +0530

    new `FormInput` component implementation

---
+++ src/components/form-elements/FormInput.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components/macro';
+
+const Input = styled.input`
+  width: 100%;
+  min-height: 50px;
+
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+
+  padding: 18px 0;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #bbbbbb;
+  margin-bottom: 12px;
+
+  &::placeholder {
+    color: rgba(6, 6, 6, 0.4);
+    opacity: 1; /* Firefox */
+  }
+
+  &:-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: rgba(6, 6, 6, 0.4);
+  }
+
+  &::-ms-input-placeholder {
+    /* Microsoft Edge */
+    color: rgba(6, 6, 6, 0.4);
+  }
+`;
+
+const FormInput = (props: React.InputHTMLAttributes<HTMLInputElement>): JSX.Element => {
+  return <Input {...props} />;
+};
+
+export default FormInput;
