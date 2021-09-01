4836e420d67aed403ae9920bbca98ca3c701dab1
SivaramPg <sivarampg95@gmail.com>
2021-09-01 15:35:01 +0530

    updated App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
+import { GlobalStyles } from 'components/common/styles/GlobalStyles';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
