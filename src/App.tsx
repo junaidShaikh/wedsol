a9c5fbcee29718ffdc9bd3f3eb652dfe3faac863
SivaramPg <sivarampg95@gmail.com>
2021-09-01 16:31:38 +0530

    `react-router-dom` integration and bring in new components and containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { GlobalStyles } from 'components/common/styles/GlobalStyles';
+
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+
+import Landing from 'containers/Landing';
 
 const App = () => {
   return (
     <React.Fragment>
       <GlobalStyles />
-      <h1>Hello World</h1>
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </Router>
+      <Footer />
     </React.Fragment>
   );
 };
