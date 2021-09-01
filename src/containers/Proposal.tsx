6b1d9a717b01b96bb4df2bbf964a5703d4d00b4e
SivaramPg <sivarampg95@gmail.com>
2021-09-01 17:14:55 +0530

    new `Proposal` container component implementation

---
+++ src/containers/Proposal.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components/macro';
+
+import SectionTitle from 'components/common/SectionTitle';
+import ConnectedAccountPill from 'components/ConnectedAccountPill';
+import ProposalStages from 'components/ProposalStages';
+
+const ProposalWrapper = styled.main`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .connected-account-pill {
+    position: fixed;
+    top: 120px;
+  }
+
+  .section-title {
+    font-size: 55px;
+    line-height: 73px;
+    margin-bottom: 64px;
+  }
+`;
+
+const Proposal = (): JSX.Element => {
+  return (
+    <ProposalWrapper>
+      <ConnectedAccountPill className="connected-account-pill" />
+      <SectionTitle className="section-title">Start a new proposal</SectionTitle>
+      <ProposalStages />
+    </ProposalWrapper>
+  );
+};
+
+export default Proposal;
