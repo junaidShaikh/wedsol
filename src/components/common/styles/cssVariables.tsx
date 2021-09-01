2f711b40f0eb715ba748efaf10d2000af70e1708
SivaramPg <sivarampg95@gmail.com>
2021-09-01 15:27:46 +0530

    css variables declaration

---
+++ src/components/common/styles/cssVariables.tsx
@@ -0,0 +1,15 @@
+import { css } from 'styled-components/macro';
+
+const cssVariables = css`
+  :root {
+    /* Font Families */
+    --pt-serif: 'PT Serif';
+    --prata: 'Prata';
+
+    /* Color Palette */
+    --main-background-color: #ffffff;
+    --main-text-color: #000000;
+  }
+`;
+
+export { cssVariables };
