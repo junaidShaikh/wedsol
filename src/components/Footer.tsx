bd8030517b61f705e7b5124e13dd9e05273d809f
SivaramPg <sivarampg95@gmail.com>
2021-09-01 16:25:24 +0530

    new `Footer` component implementation

---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components/macro';
+
+import FlexRowWrapper from 'components/common/wrappers/FlexRowWrapper';
+
+import poweredBySolana from 'assets/logos/powered-by-solana.png';
+
+const FooterWrapper = styled.footer`
+  width: 100%;
+  height: 120px;
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  & > ${FlexRowWrapper} {
+    width: 100%;
+    height: 100%;
+
+    align-items: center;
+    justify-content: center;
+
+    .powered-by-solana {
+      max-width: 242px;
+    }
+  }
+`;
+
+const Footer = (): JSX.Element => {
+  return (
+    <FooterWrapper>
+      <FlexRowWrapper>
+        <img src={poweredBySolana} alt="Powered by Solana" className="powered-by-solana" />
+      </FlexRowWrapper>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
