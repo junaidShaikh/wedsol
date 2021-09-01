5c7a70a4760705403853c751eb2d87e7399f8eb8
SivaramPg <sivarampg95@gmail.com>
2021-09-01 15:28:17 +0530

    `GlobalStyles` declarations file

---
+++ src/components/common/styles/GlobalStyles.tsx
@@ -0,0 +1,47 @@
+import { css, createGlobalStyle } from 'styled-components/macro';
+
+import { cssVariables } from './cssVariables';
+
+const globalCss = css`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    user-select: none;
+    -webkit-user-select: none; /* disable text select */
+    -webkit-touch-callout: none; /* disable callout, image save panel (popup) */
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-tap-highlight-color: transparent; /* "turn off" link highlight */
+  }
+
+  body {
+    box-sizing: border-box;
+    font-family: 'PT Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+      'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    background: var(--main-background-color);
+    color: var(--main-text-color);
+  }
+
+  h1,
+  h2,
+  h3 {
+    text-rendering: optimizeLegibility;
+  }
+
+  a:focus {
+    outline: 0; // Firefox (remove border on link click)
+  }
+`;
+
+const GlobalStyles = createGlobalStyle`
+  ${cssVariables}
+  ${globalCss};
+`;
+
+export { GlobalStyles };
