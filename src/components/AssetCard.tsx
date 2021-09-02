30f4fa6ea441ceb17a142ed05ca581d9563e3c8b
SivaramPg <sivarampg95@gmail.com>
2021-09-02 21:17:47 +0530

    boilerplate component file

---
+++ src/components/AssetCard.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro';
+
+const AssetCardWrapper = styled.div``;
+
+interface AssetCardProps {}
+
+const AssetCard = ({}: AssetCardProps): JSX.Element => {
+  return <AssetCardWrapper></AssetCardWrapper>;
+};
+
+export default AssetCard;
