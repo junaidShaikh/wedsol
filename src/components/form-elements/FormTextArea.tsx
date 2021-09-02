ceb559e615467a353cd32af272e3361f374a1cf4
SivaramPg <sivarampg95@gmail.com>
2021-09-02 21:09:28 +0530

    new `FormTextArea` component implementation

---
+++ src/components/form-elements/FormTextArea.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+const TextArea = styled.textarea`
+  width: 100%;
+  min-height: 50px;
+
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+
+  font-family: 'Inter';
+  padding: 18px 0;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #bbbbbb;
+  margin-bottom: 12px;
+
+  &::placeholder {
+    color: rgba(6, 6, 6, 0.4);
+    opacity: 1; /* Firefox */
+  }
+
+  &:-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: rgba(6, 6, 6, 0.4);
+  }
+
+  &::-ms-input-placeholder {
+    /* Microsoft Edge */
+    color: rgba(6, 6, 6, 0.4);
+  }
+`;
+
+const FormTextArea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
+  (props, ref): JSX.Element => {
+    return <TextArea {...props} ref={ref} />;
+  }
+);
+
+export default FormTextArea;
