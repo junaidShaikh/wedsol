1ce81bcfe39cb4271ae40ecabf0c33de773db264
SivaramPg <sivarampg95@gmail.com>
2021-09-02 11:21:44 +0530

    `Landing` container component conditionally render button

--- src/containers/Landing.tsx
+++ src/containers/Landing.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components/macro';
+import { useSnapshot } from 'valtio';
+import { useHistory } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
+
+import { state } from 'state';
 
 import SectionTitle from 'components/common/SectionTitle';
 import ConnectWalletButton from 'components/ConnectWalletButton';
+import SolidButton from 'components/common/SolidButton';
 
 const LandingWrapper = styled.main`
   width: 100%;
@@ -17,13 +23,43 @@ const LandingWrapper = styled.main`
     line-height: 73px;
     margin-bottom: 53px;
   }
+
+  .solid-button {
+    width: 270px;
+    position: relative;
+
+    display: grid;
+    place-items: center;
+
+    svg {
+      width: 32px;
+      height: 32px;
+      position: absolute;
+      right: 24px;
+    }
+  }
 `;
 
 const Landing = (): JSX.Element => {
+  const history = useHistory();
+  const snap = useSnapshot(state);
+
   return (
     <LandingWrapper>
       <SectionTitle className="section-title">Blockchain Marriage Certificates</SectionTitle>
-      <ConnectWalletButton />
+      {snap.isWalletConnected ? (
+        <SolidButton
+          className="solid-button"
+          onClick={() => {
+            history.push('/proposal');
+          }}
+        >
+          Proceed
+          <BsArrowRight />
+        </SolidButton>
+      ) : (
+        <ConnectWalletButton />
+      )}
     </LandingWrapper>
   );
 };
