1f2e1c16468a081a85d616c3de00addb0bdc4ae1
SivaramPg <sivarampg95@gmail.com>
2021-09-02 21:11:24 +0530

    `react-hook-form` integration to functionalize form component

--- src/components/forms/AddAssetForm.tsx
+++ src/components/forms/AddAssetForm.tsx
@@ -1,14 +1,36 @@
 import styled from 'styled-components/macro';
 import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 import Container from 'components/common/wrappers/Container';
 import FlexRowWrapper from 'components/common/wrappers/FlexRowWrapper';
 import FlexColumnWrapper from 'components/common/wrappers/FlexColumnWrapper';
 import FormInput from 'components/form-elements/FormInput';
+import FormTextArea from 'components/form-elements/FormTextArea';
 import SolidButton from 'components/common/SolidButton';
 import AddImages from 'components/form-elements/AddImages';
 import ProposalLink from 'components/ProposalLink';
 import SignerCard from 'components/SignerCard';
+
+const defaultValues = {
+  assetName: '',
+  assetDescription: '',
+  assetValue: '',
+  assetTerms: '',
+  percentageSplit: '',
+  percentageIncaseOfDivorce: '',
+};
+
+const validationSchema = Yup.object({
+  assetName: Yup.string().required(),
+  assetDescription: Yup.string().required(),
+  assetValue: Yup.number().positive().integer().required(),
+  assetTerms: Yup.string().required(),
+  percentageSplit: Yup.string().required(),
+  percentageIncaseOfDivorce: Yup.string().required(),
+}).required();
 
 const AddAssetFormWrapper = styled.div`
   width: 100%;
@@ -58,22 +80,44 @@ const AddAssetFormWrapper = styled.div`
 `;
 
 const AddAssetForm = (): JSX.Element => {
+  const { register, handleSubmit } = useForm({
+    defaultValues,
+    resolver: yupResolver(validationSchema),
+  });
+
   const history = useHistory();
+
+  const onSubmit = (d: any) => {
+    console.log(d);
+    history.push('/approve-asset');
+  };
 
   return (
     <AddAssetFormWrapper>
       <Container>
         <FlexRowWrapper>
           <FlexColumnWrapper className="col-1">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <AddImages label="Add Images" />
-              <FormInput placeholder="Asset Name" />
-              <FormInput placeholder="Asset Description" />
-              <FormInput placeholder="Asset Value" />
-              <FormInput placeholder="Asset Terms" />
-              <FormInput placeholder="Percentage Split" />
-              <FormInput placeholder="Incase of Divorce" />
-              <SolidButton onClick={() => history.push('/approve-asset')}>SIGN ASSET</SolidButton>
+              <FormInput placeholder="Asset Name" {...register('assetName')} />
+              <FormTextArea placeholder="Asset Description" {...register('assetDescription')} />
+              <FormInput type="number" placeholder="Asset Value" {...register('assetValue')} />
+              <FormTextArea placeholder="Asset Terms" {...register('assetTerms')} />
+              <FormInput
+                type="number"
+                min={0}
+                max={100}
+                placeholder="Percentage Split"
+                {...register('percentageSplit')}
+              />
+              <FormInput
+                type="number"
+                min={0}
+                max={100}
+                placeholder="Percentage Incase of Divorce"
+                {...register('percentageIncaseOfDivorce')}
+              />
+              <SolidButton type="submit">SIGN ASSET</SolidButton>
             </form>
           </FlexColumnWrapper>
           <FlexColumnWrapper className="col-2">
