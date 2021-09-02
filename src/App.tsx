41c0b135440d917aa761add82e5d481cce32767f
SivaramPg <sivarampg95@gmail.com>
2021-09-02 11:24:49 +0530

    bring in new containers as routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ import Landing from 'containers/Landing';
 const Proposal = React.lazy(() => import('containers/Proposal'));
 const SendNFTRing = React.lazy(() => import('containers/SendNFTRing'));
 const SuccessfullyMinted = React.lazy(() => import('containers/SuccessfullyMinted'));
+const AcceptRingRequest = React.lazy(() => import('containers/AcceptRingRequest'));
+const AcceptingRing = React.lazy(() => import('containers/AcceptingRing'));
+const Engagement = React.lazy(() => import('containers/Engagement'));
 
 const App = () => {
   return (
@@ -27,6 +30,9 @@ const App = () => {
             <ProtectedRoute exact path="/proposal" component={Proposal} />
             <ProtectedRoute exact path="/send-nft-ring" component={SendNFTRing} />
             <ProtectedRoute exact path="/successful-mint" component={SuccessfullyMinted} />
+            <Route exact path="/accept-ring-request" component={AcceptRingRequest} />
+            <ProtectedRoute exact path="/accepting-ring" component={AcceptingRing} />
+            <Route exact path="/engagement" component={Engagement} />
           </Switch>
         </Router>
       </React.Suspense>
