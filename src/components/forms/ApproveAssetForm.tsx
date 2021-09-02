8cb3680061603448998fc0dd6073ab8940e3df30
SivaramPg <sivarampg95@gmail.com>
2021-09-02 21:12:54 +0530

    boilerplate component files

---
+++ src/components/forms/ApproveAssetForm.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro';
+
+const ApproveAssetFormWrapper = styled.div``;
+
+interface ApproveAssetFormProps {}
+
+const ApproveAssetForm = ({}: ApproveAssetFormProps): JSX.Element => {
+  return <ApproveAssetFormWrapper></ApproveAssetFormWrapper>;
+};
+
+export default ApproveAssetForm;
