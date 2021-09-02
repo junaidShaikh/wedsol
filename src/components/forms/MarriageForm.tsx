6a5cafb598477dbcb1d98ca3dea19e559758b5db
SivaramPg <sivarampg95@gmail.com>
2021-09-02 21:11:31 +0530

    `react-hook-form` integration to functionalize form component

--- src/components/forms/MarriageForm.tsx
+++ src/components/forms/MarriageForm.tsx
@@ -1,12 +1,32 @@
 import styled from 'styled-components/macro';
 import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 import Container from 'components/common/wrappers/Container';
 import FlexRowWrapper from 'components/common/wrappers/FlexRowWrapper';
 import FlexColumnWrapper from 'components/common/wrappers/FlexColumnWrapper';
 import MarriagePreview from 'components/MarriagePreview';
 import FormInput from 'components/form-elements/FormInput';
+import FormTextArea from 'components/form-elements/FormTextArea';
 import SolidButton from 'components/common/SolidButton';
+
+const defaultValues = {
+  marriageDate: '',
+  proposerName: '',
+  spouseName: '',
+  proposerVows: '',
+  spouseVows: '',
+};
+
+const validationSchema = Yup.object({
+  marriageDate: Yup.string().required(),
+  proposerName: Yup.string().required(),
+  spouseName: Yup.string().required(),
+  proposerVows: Yup.string().required(),
+  spouseVows: Yup.string().required(),
+}).required();
 
 const MarriageFormWrapper = styled.div`
   width: 100%;
@@ -51,22 +71,32 @@ const MarriageFormWrapper = styled.div`
 `;
 
 const MarriageForm = (): JSX.Element => {
+  const { register, handleSubmit } = useForm({
+    defaultValues,
+    resolver: yupResolver(validationSchema),
+  });
+
   const history = useHistory();
+
+  const onSubmit = (d: any) => {
+    console.log(d);
+    history.push('/marriage');
+  };
 
   return (
     <MarriageFormWrapper>
       <Container>
         <FlexRowWrapper>
           <FlexColumnWrapper className="col-1">
-            <form>
-              <FormInput placeholder="Pick a date" />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormInput type="datetime-local" placeholder="Pick a date" {...register('marriageDate')} />
               <FlexRowWrapper>
-                <FormInput placeholder="Your name" style={{ marginRight: 10 }} />
-                <FormInput placeholder="Your spouse's name" style={{ marginLeft: 10 }} />
+                <FormInput placeholder="Your name" style={{ marginRight: 10 }} {...register('proposerName')} />
+                <FormInput placeholder="Your spouse's name" style={{ marginLeft: 10 }} {...register('spouseName')} />
               </FlexRowWrapper>
-              <FormInput placeholder="Your vows" />
-              <FormInput placeholder="Your spouse's vows" />
-              <SolidButton onClick={() => history.push('/marriage')}>CREATE CONTRACT</SolidButton>
+              <FormTextArea placeholder="Your vows" {...register('proposerVows')} />
+              <FormTextArea placeholder="Your spouse's vows" {...register('spouseVows')} />
+              <SolidButton type="submit">CREATE CONTRACT</SolidButton>
             </form>
           </FlexColumnWrapper>
           <FlexColumnWrapper className="col-2">
