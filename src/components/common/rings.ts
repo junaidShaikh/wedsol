99ec11ec795c230cc127324a9e9e7cbc3c0f4c1b
SivaramPg <sivarampg95@gmail.com>
2021-09-03 10:53:02 +0530

    rename `rings` file and replace array values with CDN urls

---
+++ src/components/common/rings.ts
@@ -0,0 +1,9 @@
+const rings = [
+  'https://2b151328-4747-4287-b97d-30fc64db91f1.sgp1.digitaloceanspaces.com/5df12d02-fe35-428f-b0ee-206ee882b673/preview-ring-1.png',
+  'https://2b151328-4747-4287-b97d-30fc64db91f1.sgp1.digitaloceanspaces.com/4452b888-aea9-44c1-8a28-64c9a898ca42/preview-ring-2.png',
+  'https://2b151328-4747-4287-b97d-30fc64db91f1.sgp1.digitaloceanspaces.com/5df12d02-fe35-428f-b0ee-206ee882b673/preview-ring-1.png',
+  'https://2b151328-4747-4287-b97d-30fc64db91f1.sgp1.digitaloceanspaces.com/4452b888-aea9-44c1-8a28-64c9a898ca42/preview-ring-2.png',
+  'https://2b151328-4747-4287-b97d-30fc64db91f1.sgp1.digitaloceanspaces.com/5df12d02-fe35-428f-b0ee-206ee882b673/preview-ring-1.png',
+];
+
+export default rings;
--- src/components/common/rings.tsx
+++
@@ -1,6 +0,0 @@
-import previewRing1 from 'assets/images/preview-ring-1.png';
-import previewRing2 from 'assets/images/preview-ring-2.png';
-
-const rings = [previewRing1, previewRing2, previewRing1, previewRing2, previewRing1];
-
-export default rings;
