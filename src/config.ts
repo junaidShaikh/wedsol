8b71fea91f18ce753aa492cef4e535f5d88a0ce7
SivaramPg <sivarampg95@gmail.com>
2021-09-03 21:12:29 +0530

    add new prop to config object

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,17 @@ import { AppConfig } from 'ts/interfaces/global.interface';
 
 const prodConfig: AppConfig = {
   apiBaseUrl: 'https://snft.ocg.technology/',
+  programId: 'BkTaBXzfJdBgwcVoMQGG5jZHSy2ZPy8jKCVzNFtajorb',
 };
 
 const stagingConfig: AppConfig = {
   apiBaseUrl: 'https://snft.ocg.technology/',
+  programId: 'BkTaBXzfJdBgwcVoMQGG5jZHSy2ZPy8jKCVzNFtajorb',
 };
 
 const localConfig: AppConfig = {
   apiBaseUrl: 'https://snft.ocg.technology/',
+  programId: 'BkTaBXzfJdBgwcVoMQGG5jZHSy2ZPy8jKCVzNFtajorb',
 };
 
 export default (function () {
