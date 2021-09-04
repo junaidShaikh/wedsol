4a436f802b9578ab343adc64dc83a939c82af146
SivaramPg <sivarampg95@gmail.com>
2021-09-04 17:33:08 +0530

    add URL param to marriage path

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App = () => {
             <Route exact path="/engagement/:proposalPubKey" component={Engagement} />
 
             <ProtectedRoute exact path="/marriage/new" component={StartNewMarriage} />
-            <ProtectedRoute exact path="/marriage" component={Marriage} />
+            <ProtectedRoute exact path="/marriage/:proposalPubKey" component={Marriage} />
             <ProtectedRoute exact path="/assets" component={Assets} />
             <ProtectedRoute exact path="/add-asset" component={AddAsset} />
             <ProtectedRoute exact path="/approve-asset" component={ApproveAsset} />
