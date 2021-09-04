8847bb7bb8146da21506a6c31c896952fbec674d
SivaramPg <sivarampg95@gmail.com>
2021-09-04 13:38:11 +0530

    updated `AppConfig` interface

--- src/ts/interfaces/global.interface.ts
+++ src/ts/interfaces/global.interface.ts
@@ -1,4 +1,7 @@
 export interface AppConfig {
   apiBaseUrl: string;
   programId: string;
+  solanaNetwork: 'mainnet-beta' | 'testnet' | 'devnet';
+  ipfsGatewayBaseUrl: string;
+  generateSolanaExplorerBaseUrl: (transaction: string) => string;
 }
