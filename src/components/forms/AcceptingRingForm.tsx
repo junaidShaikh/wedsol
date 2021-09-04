9a8de7245244d7c5324056b8723a432f3dd0219a
SivaramPg <sivarampg95@gmail.com>
2021-09-04 16:14:13 +0530

    `accept wedding` transaction implementation

--- src/components/forms/AcceptingRingForm.tsx
+++ src/components/forms/AcceptingRingForm.tsx
@@ -4,6 +4,11 @@ import { useHistory, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
+import { PublicKey, TransactionInstruction, Transaction } from '@solana/web3.js';
+import { Buffer } from 'buffer';
+import { useSnapshot } from 'valtio';
+
+import { state } from 'state';
 
 import Container from 'components/common/wrappers/Container';
 import FlexRowWrapper from 'components/common/wrappers/FlexRowWrapper';
@@ -11,8 +16,13 @@ import FlexColumnWrapper from 'components/common/wrappers/FlexColumnWrapper';
 import AcceptingRingPreview from 'components/AcceptingRingPreview';
 import SolidButton from 'components/common/SolidButton';
 import RingSelect from 'components/form-elements/RingSelect';
+import Spinner from 'components/common/Spinner';
 
 import rings from 'components/common/rings';
+
+import { getProvider } from 'utils/getProvider';
+import { uploadJsonToIpfs } from 'apis/ipfs';
+import config from 'config';
 
 const defaultValues = {
   spouseRing: 0,
@@ -55,8 +65,19 @@ const AcceptingRingFormWrapper = styled.div`
         }
       }
 
-      button {
+      button.solid-button {
         margin-top: 175px;
+        position: relative;
+
+        display: grid;
+        place-items: center;
+
+        .spinner {
+          width: 30px;
+          height: 30px;
+          position: absolute;
+          left: 15px;
+        }
       }
     }
   }
@@ -91,6 +112,8 @@ const AcceptingRingForm = ({
   proposerRing,
   qrCodeString,
 }: AcceptingRingFormProps): JSX.Element => {
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+
   const { proposalPubKey } = useParams<{ proposalPubKey: string }>();
 
   const { watch, handleSubmit, setValue } = useForm({
@@ -101,16 +124,77 @@ const AcceptingRingForm = ({
 
   const history = useHistory();
 
+  const { connection, proposalInfo } = useSnapshot(state);
+
   React.useEffect(() => {
     if (!proposalPubKey) {
-      history.replace('/');
+      return history.replace('/');
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const onSubmit = (d: any) => {
-    console.log(d);
-    history.push(`/engagement/${proposalPubKey}`);
+  const onSubmit = async (d: typeof defaultValues) => {
+    try {
+      setIsSubmitting(true);
+      console.log(d);
+
+      // Upload JSON to IPFS and get IPFS CID
+      const { data } = await uploadJsonToIpfs({ ...proposalInfo.data, spouseRing: rings[d.spouseRing] });
+
+      if (data) {
+        console.log(data);
+
+        const provider = getProvider();
+
+        if (!provider?.publicKey) return;
+
+        const programIdPublicKey = new PublicKey(config.programId);
+
+        let weddingData = '';
+        weddingData += '\x04';
+
+        const instruction = new TransactionInstruction({
+          keys: [
+            {
+              pubkey: provider.publicKey as PublicKey,
+              isSigner: true,
+              isWritable: false,
+            },
+            {
+              pubkey: new PublicKey(proposalPubKey),
+              isSigner: false,
+              isWritable: true,
+            },
+          ],
+          programId: programIdPublicKey,
+          data: Buffer.from(weddingData, 'utf-8'),
+        });
+        const transaction = new Transaction().add(instruction);
+        transaction.feePayer = provider.publicKey;
+        console.log('Getting recent blockhash');
+        (transaction as any).recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+        if (transaction) {
+          let signed = await provider.signTransaction(transaction);
+          console.log('Got signature, submitting transaction');
+          let signature = await connection.sendRawTransaction(signed.serialize());
+          console.log('Submitted transaction ' + signature + ', awaiting confirmation');
+          // ! This line throws error and will not allow program to execute further.
+          // await connection.confirmTransaction(signature);
+          console.log('Transaction ' + signature + ' confirmed');
+          history.push({
+            pathname: `/engagement/${proposalPubKey}`,
+          });
+        }
+      }
+    } catch (error: any) {
+      console.warn(error);
+      if (error?.code === 4001 && error?.message === 'User rejected the request.') {
+        alert(error?.message);
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
@@ -123,7 +207,10 @@ const AcceptingRingForm = ({
                 Choose a Ring for <span>{proposerName}</span>
               </h2>
               <RingSelect label="Pick a ring" onChange={(value) => setValue('spouseRing', value)} />
-              <SolidButton type="submit">MINT AND ACCEPT</SolidButton>
+              <SolidButton type="submit" className="solid-button">
+                {isSubmitting && <Spinner className="spinner" />}
+                {isSubmitting ? 'Minting...' : 'MINT AND ACCEPT'}
+              </SolidButton>
             </form>
           </FlexColumnWrapper>
           <FlexColumnWrapper className="col-2">
