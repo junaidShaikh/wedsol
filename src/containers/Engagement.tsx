b984f14858747df6a5ab4984a97758afd8ec7a49
SivaramPg <sivarampg95@gmail.com>
2021-09-04 16:13:11 +0530

    dynamic data integration with fetching from blockchain & IPFS

--- src/containers/Engagement.tsx
+++ src/containers/Engagement.tsx
@@ -1,15 +1,24 @@
+import * as React from 'react';
 import styled from 'styled-components/macro';
+import { PublicKey } from '@solana/web3.js';
+import { useParams, useHistory } from 'react-router-dom';
+import axios from 'axios';
 import { useSnapshot } from 'valtio';
+import { Buffer } from 'buffer';
 import { IoLink, IoLogoTwitter, IoLogoFacebook } from 'react-icons/io5';
 
+import config from 'config';
 import { state } from 'state';
 
 import Container from 'components/common/wrappers/Container';
 import FlexColumnWrapper from 'components/common/wrappers/FlexColumnWrapper';
 import FlexRowWrapper from 'components/common/wrappers/FlexRowWrapper';
 import EngagementCard from 'components/EngagementCard';
+import FullPageSpinner from 'components/common/FullPageSpinner';
 
 import rings from 'components/common/rings';
+
+import shortenWalletAddress from 'utils/shortenWalletAddress';
 
 const EngagementWrapper = styled.main`
   width: 100%;
@@ -52,6 +61,64 @@ const EngagementWrapper = styled.main`
 
 const Engagement = (): JSX.Element => {
   const snap = useSnapshot(state);
+
+  const { proposalPubKey } = useParams<{ proposalPubKey: string }>();
+  const history = useHistory();
+
+  React.useEffect(() => {
+    try {
+      if (!proposalPubKey) {
+        return history.replace('/');
+      }
+
+      (async () => {
+        if (!state.proposalInfo.data) {
+          state.proposalInfo.isLoading = true;
+          const programPubKey = new PublicKey(proposalPubKey);
+          const accountInfo = await snap.connection.getAccountInfo(programPubKey);
+          if (accountInfo === null) {
+            throw new Error('Error: cannot find the greeted account');
+          } else {
+            const endPost = accountInfo.data.indexOf(0);
+            const result = JSON.parse(accountInfo.data.slice(0, endPost).toString());
+
+            console.log(result);
+
+            const { data } = await axios.get(`${config.ipfsGatewayBaseUrl}${result?.info?.substr(0, 46)}`);
+
+            if (data) {
+              state.proposalInfo.isLoading = false;
+              state.proposalInfo.isSuccess = true;
+              state.proposalInfo.data = {
+                ...data,
+                signers: [
+                  !result.partner1.every((value: number) => value === 0)
+                    ? shortenWalletAddress(new PublicKey(Buffer.from(result.partner1)).toBase58(), 5)
+                    : null,
+                  !result.partner2.every((value: number) => value === 0)
+                    ? shortenWalletAddress(new PublicKey(Buffer.from(result.partner2)).toBase58(), 5)
+                    : null,
+                ],
+              };
+            } else {
+              state.proposalInfo.isLoading = false;
+              state.proposalInfo.isSuccess = false;
+            }
+          }
+        }
+      })();
+    } catch (error) {
+      console.log(error);
+      state.proposalInfo.isLoading = false;
+      state.proposalInfo.isSuccess = false;
+      state.proposalInfo.error = error;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (snap.proposalInfo.isLoading) {
+    return <FullPageSpinner />;
+  }
 
   return (
     <EngagementWrapper>
