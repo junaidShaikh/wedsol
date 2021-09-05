55ad8678993bdd245a7ed80cc4b8dc7a42b55f91
SivaramPg <sivarampg95@gmail.com>
2021-09-05 12:59:32 +0530

    dynamic data integration to fetch from blockchain and IPFS and set conditionals accordingly

--- src/containers/Proposal.tsx
+++ src/containers/Proposal.tsx
@@ -1,8 +1,25 @@
+import * as React from 'react';
 import styled from 'styled-components/macro';
+import { useSnapshot } from 'valtio';
+
+import {
+  state,
+  setProposalInfoLoading,
+  setProposalInfoData,
+  setProposalInfoFailure,
+  setProposalInfoError,
+} from 'state';
 
 import ConnectedAccountPill from 'components/ConnectedAccountPill';
 import SectionTitle from 'components/common/SectionTitle';
 import ProposalStages from 'components/ProposalStages';
+import FullPageSpinner from 'components/common/FullPageSpinner';
+
+import getPubKeyFromSeed from 'utils/getPubKeyFromSeed';
+import getAccountInfo from 'utils/getAccountInfo';
+import getActualSigners from 'utils/getActualSigners';
+import { fetchIpfsJsonData } from 'apis/ipfs';
+import shortenWalletAddress from 'utils/shortenWalletAddress';
 
 const ProposalWrapper = styled.main`
   width: 100%;
@@ -27,11 +44,52 @@ const ProposalWrapper = styled.main`
 `;
 
 const Proposal = (): JSX.Element => {
+  const [disableProposalFlow, setDisableProposalFlow] = React.useState(false);
+  const [disableMarriageFlow, setDisableMarriageFlow] = React.useState(false);
+
+  const snap = useSnapshot(state);
+
+  React.useEffect(() => {
+    try {
+      (async () => {
+        setProposalInfoLoading();
+        setDisableProposalFlow(false);
+
+        const proposalPubKey = await getPubKeyFromSeed();
+        const accountInfo = await getAccountInfo(proposalPubKey);
+        if (accountInfo === null) {
+          setProposalInfoFailure();
+          setDisableMarriageFlow(true);
+          return;
+        }
+        const { data } = await fetchIpfsJsonData(accountInfo?.extra?.substr(0, 46));
+        if (data) {
+          setProposalInfoData({
+            ...data,
+            signers: getActualSigners([accountInfo.partner1, accountInfo.partner2]).map((signer) =>
+              shortenWalletAddress(signer, 5)
+            ),
+          });
+        } else {
+          setProposalInfoFailure();
+        }
+      })();
+    } catch (error) {
+      console.log(error);
+      setProposalInfoError(error);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (snap.proposalInfo.isLoading) {
+    return <FullPageSpinner />;
+  }
+
   return (
     <ProposalWrapper>
       <ConnectedAccountPill className="connected-account-pill" />
       <SectionTitle className="section-title">Start a new proposal</SectionTitle>
-      <ProposalStages />
+      <ProposalStages disableProposalFlow={disableProposalFlow} disableMarriageFlow={disableMarriageFlow} />
     </ProposalWrapper>
   );
 };
