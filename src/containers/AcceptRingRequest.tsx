298ff3cf5312048027f15cebfb9f810f7598ae50
SivaramPg <sivarampg95@gmail.com>
2021-09-05 10:24:44 +0530

    refactor components to use new utility methods and remove log statements

--- src/containers/AcceptRingRequest.tsx
+++ src/containers/AcceptRingRequest.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import styled from 'styled-components/macro';
 import { PublicKey } from '@solana/web3.js';
 import { useParams, useHistory } from 'react-router-dom';
-import axios from 'axios';
 import { useSnapshot } from 'valtio';
-import { Buffer } from 'buffer';
 
-import config from 'config';
-import { state } from 'state';
+import {
+  state,
+  setProposalInfoLoading,
+  setProposalInfoData,
+  setProposalInfoFailure,
+  setProposalInfoError,
+} from 'state';
 
 import ConnectedAccountPill from 'components/ConnectedAccountPill';
 import Container from 'components/common/wrappers/Container';
@@ -16,8 +19,10 @@ import FullPageSpinner from 'components/common/FullPageSpinner';
 
 import rings from 'components/common/rings';
 
+import getAccountInfo from 'utils/getAccountInfo';
+import getActualSigners from 'utils/getActualSigners';
+import { fetchIpfsJsonData } from 'apis/ipfs';
 import shortenWalletAddress from 'utils/shortenWalletAddress';
-import getConnection from 'utils/getConnection';
 
 const AcceptRingRequestWrapper = styled.main`
   width: 100%;
@@ -45,8 +50,6 @@ const AcceptRingRequest = (): JSX.Element => {
   const { proposalPubKey } = useParams<{ proposalPubKey: string }>();
   const history = useHistory();
 
-  const connection = getConnection();
-
   React.useEffect(() => {
     try {
       if (!proposalPubKey) {
@@ -54,44 +57,25 @@ const AcceptRingRequest = (): JSX.Element => {
       }
 
       (async () => {
-        if (!state.proposalInfo.data) {
-          state.proposalInfo.isLoading = true;
-          const programPubKey = new PublicKey(proposalPubKey);
-          const accountInfo = await connection.getAccountInfo(programPubKey);
-          if (accountInfo === null) {
-            throw new Error('Error: cannot find the greeted account');
-          } else {
-            const endPost = accountInfo.data.indexOf(0);
-            const result = JSON.parse(accountInfo.data.slice(0, endPost).toString());
-
-            const { data } = await axios.get(`${config.ipfsGatewayBaseUrl}${result?.info?.substr(0, 46)}`);
-
-            if (data) {
-              state.proposalInfo.isLoading = false;
-              state.proposalInfo.isSuccess = true;
-              state.proposalInfo.data = {
-                ...data,
-                signers: [
-                  !result.partner1.every((value: number) => value === 0)
-                    ? shortenWalletAddress(new PublicKey(Buffer.from(result.partner1)).toBase58(), 5)
-                    : null,
-                  !result.partner2.every((value: number) => value === 0)
-                    ? shortenWalletAddress(new PublicKey(Buffer.from(result.partner2)).toBase58(), 5)
-                    : null,
-                ],
-              };
-            } else {
-              state.proposalInfo.isLoading = false;
-              state.proposalInfo.isSuccess = false;
-            }
-          }
+        setProposalInfoLoading();
+        const accountInfo = await getAccountInfo(new PublicKey(proposalPubKey));
+        console.log(accountInfo);
+        const { data } = await fetchIpfsJsonData(accountInfo?.extra?.substr(0, 46));
+        console.log(data);
+        if (data) {
+          setProposalInfoData({
+            ...data,
+            signers: getActualSigners([accountInfo.partner1, accountInfo.partner2]).map((signer) =>
+              shortenWalletAddress(signer, 5)
+            ),
+          });
+        } else {
+          setProposalInfoFailure();
         }
       })();
     } catch (error) {
       console.log(error);
-      state.proposalInfo.isLoading = false;
-      state.proposalInfo.isSuccess = false;
-      state.proposalInfo.error = error;
+      setProposalInfoError(error);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
--- src/containers/Engagement.tsx
+++ src/containers/Engagement.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 import styled from 'styled-components/macro';
 import { PublicKey } from '@solana/web3.js';
 import { useParams, useHistory } from 'react-router-dom';
-import axios from 'axios';
 import { useSnapshot } from 'valtio';
-import { Buffer } from 'buffer';
 import { IoLink, IoLogoTwitter, IoLogoFacebook } from 'react-icons/io5';
 
-import config from 'config';
-import { state } from 'state';
+import {
+  state,
+  setProposalInfoLoading,
+  setProposalInfoData,
+  setProposalInfoFailure,
+  setProposalInfoError,
+} from 'state';
 
 import Container from 'components/common/wrappers/Container';
 import FlexColumnWrapper from 'components/common/wrappers/FlexColumnWrapper';
@@ -18,8 +21,10 @@ import FullPageSpinner from 'components/common/FullPageSpinner';
 
 import rings from 'components/common/rings';
 
+import getAccountInfo from 'utils/getAccountInfo';
+import getActualSigners from 'utils/getActualSigners';
+import { fetchIpfsJsonData } from 'apis/ipfs';
 import shortenWalletAddress from 'utils/shortenWalletAddress';
-import getConnection from 'utils/getConnection';
 
 const EngagementWrapper = styled.main`
   width: 100%;
@@ -61,12 +66,10 @@ const EngagementWrapper = styled.main`
 `;
 
 const Engagement = (): JSX.Element => {
-  const snap = useSnapshot(state);
-
   const { proposalPubKey } = useParams<{ proposalPubKey: string }>();
   const history = useHistory();
 
-  const connection = getConnection();
+  const snap = useSnapshot(state);
 
   React.useEffect(() => {
     try {
@@ -75,46 +78,25 @@ const Engagement = (): JSX.Element => {
       }
 
       (async () => {
-        if (!state.proposalInfo.data) {
-          state.proposalInfo.isLoading = true;
-          const programPubKey = new PublicKey(proposalPubKey);
-          const accountInfo = await connection.getAccountInfo(programPubKey);
-          if (accountInfo === null) {
-            throw new Error('Error: cannot find the greeted account');
-          } else {
-            const endPost = accountInfo.data.indexOf(0);
-            const result = JSON.parse(accountInfo.data.slice(0, endPost).toString());
-
-            console.log(result);
-
-            const { data } = await axios.get(`${config.ipfsGatewayBaseUrl}${result?.info?.substr(0, 46)}`);
-
-            if (data) {
-              state.proposalInfo.isLoading = false;
-              state.proposalInfo.isSuccess = true;
-              state.proposalInfo.data = {
-                ...data,
-                signers: [
-                  !result.partner1.every((value: number) => value === 0)
-                    ? shortenWalletAddress(new PublicKey(Buffer.from(result.partner1)).toBase58(), 5)
-                    : null,
-                  !result.partner2.every((value: number) => value === 0)
-                    ? shortenWalletAddress(new PublicKey(Buffer.from(result.partner2)).toBase58(), 5)
-                    : null,
-                ],
-              };
-            } else {
-              state.proposalInfo.isLoading = false;
-              state.proposalInfo.isSuccess = false;
-            }
-          }
+        setProposalInfoLoading();
+        const accountInfo = await getAccountInfo(new PublicKey(proposalPubKey));
+        console.log(accountInfo);
+        const { data } = await fetchIpfsJsonData(accountInfo?.extra?.substr(0, 46));
+        console.log(data);
+        if (data) {
+          setProposalInfoData({
+            ...data,
+            signers: getActualSigners([accountInfo.partner1, accountInfo.partner2]).map((signer) =>
+              shortenWalletAddress(signer, 5)
+            ),
+          });
+        } else {
+          setProposalInfoFailure();
         }
       })();
     } catch (error) {
       console.log(error);
-      state.proposalInfo.isLoading = false;
-      state.proposalInfo.isSuccess = false;
-      state.proposalInfo.error = error;
+      setProposalInfoError(error);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
