def2d33a7cec6a9669f2381677a94d104648dec6
SivaramPg <sivarampg95@gmail.com>
2021-09-05 10:17:21 +0530

    new IPFS method to fetch JSON data using IPFS CID

--- src/apis/ipfs.ts
+++ src/apis/ipfs.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import config from 'config';
 
 export const uploadJsonToIpfs = (props: {}) => {
   return axios.post<{ cid: string }>('https://snft.ocg.technology/ipfs', props);
+};
+
+export const fetchIpfsJsonData = (ipfsCid: string) => {
+  return axios.get(`${config.ipfsGatewayBaseUrl}${ipfsCid}`);
 };
